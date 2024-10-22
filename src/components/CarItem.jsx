c6a691f8f8ed467484be71251c3742d78f28afbb
KiptooMannu <Kkgg7241@gmail.com>
2024-10-22 11:59:23 +0300

    changes

--- src/components/CarItem.jsx
+++ src/components/CarItem.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-function CarItem({Car}) {
+function CarItem({car}) {
   return (
     <div>
-      <img src={Car?.image} height={250} width={300}/>
+      <img src={car?.image} height={250} width={300}/>
+   <div className='p-4'>
+    <h2 className='font-bold text-black text-lg mb-2'>{car?.name}</h2>
+   </div>
     </div>
+
   )
 }
 
