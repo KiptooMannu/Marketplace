6afad0f0fd03e7b6536826c9844c9a59c4f85b81
KiptooMannu <Kkgg7241@gmail.com>
2024-10-22 23:50:32 +0300

    changes

---
+++ src/add-listing/component/CheckBoxField.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CheckBoxField() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CheckBoxField
--- src/add-listing/index.jsx
+++ src/add-listing/index.jsx
@@ -3,6 +3,9 @@ import Header from '@/components/Header'
 import carDetails from './../Shared/carDetails.json'
 import InputField from './component/InputField'
 import Dropdownfield from './component/Dropdownfield'
+import TextAreaField from './component/TextAreaField'
+import { Separator } from '@radix-ui/react-select'
+
 function Addlisting() {
   return (
     <div>
@@ -18,13 +21,19 @@ function Addlisting() {
               {carDetails.carDetails.map((item,index)=>(
                   <div key = {index}>
                     <label className='text-sm'>{item?.label}{item.required&&<span className='text-red-800'>*</span>}</label>
-                    {item.fieldType==='text' || item.fieldType=== 'number'?<InputField item={item}/>:item.fieldType=='dropdown'?<Dropdownfield item={item}/> :null}
+                    {item.fieldType==='text' || item.fieldType=== 'number'?<InputField item={item}/>
+                    :item.fieldType=='dropdown'?<Dropdownfield item={item}/> 
+                    :item.fieldType=='textarea'?<TextAreaField item={item}/> :null}
                     </div>
 
               ))}
             </div>
           </div>
+          <Separator className='my-6'/>
           {/* Featureslist */}
+          <div>
+            <h2  className='font-medium text-xl my-6 '>Features</h2>
+          </div>
           {/* Car Images */}
         </form>
       </div>
