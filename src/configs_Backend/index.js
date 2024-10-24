ca68508c194598cd48ec2bd1241d9cf6e6552e2b
KiptooMannu <Kkgg7241@gmail.com>
2024-10-24 12:56:24 +0300

    changes

--- src/configs_Backend/index.js
+++ src/configs_Backend/index.js
@@ -1,4 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 const sql = neon(process.env.DATABASE_URL);
+import * as schema from './Schema'
 export const db = drizzle({ client: sql });
