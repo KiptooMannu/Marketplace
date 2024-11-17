8c9a546584c861afdad8adcbd774c1eb13b16f69
KiptooMannu <Kkgg7241@gmail.com>
2024-11-17 20:36:37 +0300

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,35 @@
-
-import { UserButton, useUser } from '@clerk/clerk-react'
+import { SignInButton, UserButton, useUser } from '@clerk/clerk-react'
 import React from 'react'
 import { Button } from './ui/button';
 import { Link } from 'react-router-dom';
 
 function Header() {
-  const {user ,isSignedIn} = useUser();
-
-  console.log("User:", user);
-  console.log("Is Signed In:", isSignedIn);
-
+    const {user,isSignedIn}=useUser();
   return (
-    <div className="shadow-sm p-5 items-center flex justify-between">
-           {/* <img src='./assets/logo.svg' width={150} height={10} /> */}
-           <h1>LOGO</h1>
-
-           <ul className='hidden md:flex gap-16'>
-         <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Home</li>
-         <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>New</li>
-         <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Search</li>
-         <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>PreOwned</li>
-     </ul>
-     
- {isSignedIn?
-       <div className='flex items-center gap-16'>
-
-        <UserButton/>
-
-        <Link to= {'/Profile'}>
-        <Button>Submit Listing</Button>
+    <div className='flex justify-between items-center shadow-sm p-5'>
+        <Link to={'/'}>
+            <img src='/logo.svg' width={150} height={100} />
         </Link>
-       
-        </div>
-        :
-        <Button>Submit Listing</Button>
-     
-} 
+        <ul className='hidden  md:flex gap-16'>
+            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Home</li>
+            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Search</li>
+            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>New</li>
+            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Preowned</li>
+        </ul>
+
+        {isSignedIn?
+            <div className='flex items-center gap-5'>
+                <UserButton/>
+                <Link to={'/profile'}>
+                    <Button>Submit Listing</Button>
+                </Link>
+            </div>
+            :
+            <SignInButton mode="modal" fallbackRedirectUrl='/profile'>
+            <Button>Submit Listing</Button>
+            </SignInButton>
+        }
+
     </div>
   )
 }
